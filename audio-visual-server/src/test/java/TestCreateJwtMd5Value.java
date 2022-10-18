c4556a2af519ffa638ff71b1fa521ef4e3b60722
admin <eldpepar@gmail.com>
2022-10-18 21:10:30 +0800

    Update TestCreateJwtMd5Value.java

--- audio-visual-server/src/test/java/TestCreateJwtMd5Value.java
+++ audio-visual-server/src/test/java/TestCreateJwtMd5Value.java
@@ -4,8 +4,8 @@ import org.springframework.util.DigestUtils;
 public class TestCreateJwtMd5Value {
     public static void main(String[] args) {
         BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
-        System.out.println(bCryptPasswordEncoder.encode("4793285CLJZ"));
-//        String md5DigestAsHex = DigestUtils.md5DigestAsHex("4793285CLJZ".getBytes());
+        System.out.println(bCryptPasswordEncoder.encode("xxx"));
+//        String md5DigestAsHex = DigestUtils.md5DigestAsHex("xxx".getBytes());
 //        System.out.println(md5DigestAsHex);
     }
 }
